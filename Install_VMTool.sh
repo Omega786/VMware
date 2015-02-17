2c0358a61bf417cecd49e3fb0fdfb4eaa25fd6c5
Manu Rios Tamez <mriostamez@users.noreply.github.com>
2015-02-17 08:57:46 -0600

    Delete Install_VMTool.sh

--- Install_VMTool.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-mkdir /mnt/cdrom && mount -o loop /dev/cdrom /mnt/cdrom/ && cd /mnt/cdrom/ && cp VMwareTools-9.8.3-2075148.tar.gz /tmp && cd /tmp/ && gunzip VMwareTools-9.8.3-2075148.tar.gz && tar -xvf VMwareTools-9.8.3-2075148.tar && cd vmware-tools-distrib/ && ./vmware-install.pl
